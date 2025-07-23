0c67a7b581d4be73659df5e10b7d54b805dadd7c
Lancer <0332212003@unjfsc.edu.pe>
2025-07-23 10:58:46 -0500

    Commit a de nuevafuncionalidad

--- app/src/main/java/com/example/grupo_03_tarea_16/menu.java
+++ app/src/main/java/com/example/grupo_03_tarea_16/menu.java
@@ -1,12 +1,10 @@
 package com.example.grupo_03_tarea_16;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 
-import com.example.grupo_03_tarea_16.apartadomenu.PropietarioFragment;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
